3b53d772c01e11822923ab2103fddc0f93aba331
Reuben Ayres <ra@sent.com>
2018-09-25 09:58:14 -0500

    started pseudocoding

---
+++ js/app.js
@@ -0,0 +1,51 @@
+/******************************************
+
+
+create hangman
+
+player vs computer
+player object
+game object
+
+
+word bank[]([]?)
+random word generate
+set word
+    make this current word?
+    track individual letters/word length
+    make word an array?
+    show blanks based on array
+
+
+user needs to input letters
+check/compare if letter is in word
+
+keep track of bad guesses
+keep track of good guesses
+
+
+"guess"
+    loop through array and check
+    replace letters that match
+    if no letters match
+        bad guess
+    else 
+        good guess
+
+what should happen on bad guess
+    display bad guesses 
+    7 bad guesses -- you lose
+    some UX/UI "Nope!"
+
+what should happen on good guess
+    letter should appear (UX?)
+    display good guesses
+    some UX/UI "sweet!"    
+
+it should tell user when they win
+
+----------MVP above this line----------
+
+hint
+
+*******************************************/
